cd16778a4075f508f081071c09e275de10fd6ad5
Jesiah McCann <jesiah@jesiah.net>
2012-04-17 13:38:31 -0400

    Model update
    Updated Model w/ unicode and custom methods
    - added refer to see where the link was clicked from
    - added location for basic location details
    
    Planning to use Local Storage on the fronted to track user session rather than backend (for the scope of this small project)

--- breviare/breviare_urls/models.py
+++ breviare/breviare_urls/models.py
@@ -5,6 +5,7 @@ from django.db import models
 	link = models.URLField(max_length=500)
 	link_short = models.URLField(max_length=500) #our shortend URL
 	created_on = models.DateTimeField(auto_now=True)
+	created_by = models.CharField(max_length=50) #IP address for now - In user auth model for the future would be user object
 	
 	def __unicode__(self):
 		return self.link
@@ -13,8 +14,13 @@ from django.db import models
 class Click(models.Model): #Stats and Analytics pertaining to the click of a link
 	link = models.ForeignKey(Link)
 	clicked_on = models.DateTimeField(auto_now=True) #so when was it clicked?
-	
+	clicked_by = models.CharField(max_length=50) # Who clicked me
+	refer = models.URLField(max_length=500, default="Direct") #whats the website or system that refered - Could be Direct 
+	location = models.CharField(max_length=200) #general location Not specific 
 	def __unicode__(self): 
-		return self.link
-	#def total_clicks(self): # add up all our clicks : o
+		return self.link+" Clicks"
+	def total_clicks(self):
+		link_clicks = Click.objects.filter(link=self.link)
+		return link_clicks #return all clicks of a link (all time)
+		
 	
