c0f5c751747a0dd7122aed3f57c1b7e7a3991a3e
Jesiah McCann <jesiah@jesiah.net>
2012-04-17 17:32:49 -0400

    Cleaned up from HTTP error
    cleaned up from HTTP not being appended previously
    Will Replace simple filter with regular expression for possible malformed http items

--- breviare/breviare_urls/admin.py
+++ breviare/breviare_urls/admin.py
@@ -4,5 +4,8 @@ from django.contrib import admin
 class LinkAdmin(admin.ModelAdmin):	
 	list_display = ('link','link_short','created_on','created_by')
 	search_fields = ['link','link_short']
-		
+
+class ClickAdmin(admin.ModelAdmin):
+	list_display = ('link','clicked_on','clicked_by')
 admin.site.register(Link, LinkAdmin)
+admin.site.register(Click, ClickAdmin)
--- breviare/breviare_urls/views.py
+++ breviare/breviare_urls/views.py
@@ -10,7 +10,7 @@ def shorten_link(request):
 	submit_link = request.GET['link']
 	if 'http://' not in submit_link:    #could be expanded to https 
 		submit_link = 'http://'+submit_link
-	link = Link.objects.get_or_create(link=submit_link) #need to save the url first before we hash it because we are basing the hash on the unique ID
+	link = Link.objects.get_or_create(link=submit_link,created_by=request.META.get("REMOTE_ADDR","")) #need to save the url first before we hash it because we are basing the hash on the unique ID
 	if not link[0].link_short: #Get OR CREATE returns a tuple of the object and a boolean value  - highlighting the object here
 		link[0].link_short = BASE_URL+baseconvert(link[0].pk,BASE10,BASE62)
 		link[0].save()
@@ -18,16 +18,16 @@ def shorten_link(request):
 	
 #What happens when we click on a link :o 
 def link_click(request):
-	try:
+	try: #Does this link we have clicked on exist?
 		short_link = Link.objects.get(link_short=request.GET['link'])
 	except:
 		return render(request,'nolink.html',content_type="text/html")
 	
-	click = Click.objects.create(link=short_link,clicked_by=request.META.get("REMOTE_ADDR",""))
-	return HttpResponseRedirect("http://"+short_link.link)
+	click = Click.objects.create(link=short_link,clicked_by=request.META.get("REMOTE_ADDR","")) #makes a link
+	return HttpResponseRedirect(short_link.link)
 
 		
-		
+	#figuring out what meta gives you what kind of data	
 	print request.META.get("HTTP_REFERER","")
 	print request.META.get("HTTP_USER_AGENT","")
 	print 'add'+request.META.get("REMOTE_ADDR","")
